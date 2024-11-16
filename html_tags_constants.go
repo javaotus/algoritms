a520e97e0137e1054f342d50307ce8cc5185ce08
javaotus <131445094+javaotus@users.noreply.github.com>
2024-11-16 03:38:45 +0100

    Create html_tags_constants.go

---
+++ html_tags_constants.go
@@ -0,0 +1,8 @@
+package main
+
+const (
+	DivTag               = "div"
+	MetaTag              = "meta"
+	MainContainerSection = "div.mxd-product-area-new-lister-view"
+	ProductSectionDiv    = "data-product-name"
+)
